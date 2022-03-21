cbed2f4f8096cfe3ca4bc71262c162f0f81e8ca3
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-03-21 13:49:19 -0300

    java

--- java-facens-lista1/src/ex001.java
+++
@@ -1,6 +0,0 @@
-public class ex001 {
-    public static void main(String args[]) {
-        System.out.println("Olá, mundo!");
-        System.out.println("Etcha, galera!");
-    }
-}
---
+++ java-facens-lista1/src/main01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class main01 {
+    public static void main(String[] args) {
+        String name;
+        Scanner exibe;
+
+        exibe = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        name = exibe.nextLine();
+        
+        System.out.println("");
+        System.out.println("Olá, " + name);
+    }
+}
