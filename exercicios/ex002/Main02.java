a3c0c5ae37c47fc8aee12b9159f7d8b26978d3eb
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-03-25 19:53:06 -0300

    char

--- exercicios/ex002/Main02.java
+++ exercicios/ex002/Main02.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 class Main02 {
   public static void main(String[] args) {
-    String letra;
+    char letra;
     Scanner exibe;
     
     exibe = new Scanner(System.in);
     
     System.out.print("Digite uma letra do alfabeto: ");
-    letra = exibe.nextLine();
+    letra = exibe.next().charAt(0);
 
     System.out.println("");
     System.out.println("Você digitou a letra " + letra);
