a1947d8671cb09f857fef63a855d55046f1cbf9e
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-03-25 22:16:25 -0300

    lista02

--- exercicios-lista02/ex001/ex001.java
+++ exercicios-lista02/ex001/ex001.java
@@ -2,7 +2,7 @@ package ex001;
 
 public class ex001 {
     public static void main(String[] args) {
-        int num = 18;
+        int num = 200;
 
         if (num >= 100 && num <= 200) {
             System.out.print("Você digitou um número entre 100 e 200.");
--- exercicios-lista02/ex002/ex002.java
+++ exercicios-lista02/ex002/ex002.java
@@ -1,5 +1,11 @@
 package ex002;
 
 public class ex002 {
+    public static void main(String[] args) {
+        int num = 27;
     
+        if (num < 3 || num > 25) {
+            System.out.println("Receba!");
+        }
+    }
 }
