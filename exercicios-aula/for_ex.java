bafb6224b0ec9c1a950c7ef39b9b2b7c239cb112
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-04-08 22:24:59 -0300

    exercicios-aula

---
+++ exercicios-aula/for_ex.java
@@ -0,0 +1,10 @@
+public class for_ex {
+    public static void main(String[] args) {
+        int num = 7, tabuada;
+
+        for (int cont = 0; cont <= 10; cont++) {
+            System.out.println("7 x " + cont + " = " + num * cont);
+        }
+
+    }
+}
